586abce76c66b730279f65df1773f3c6f8795ebf
Ryan Wallace <ryancwallace@outlook.com>
2020-09-30 15:30:36 -0500

    not-reddit

--- db/get_posts.sql
+++ db/get_posts.sql
@@ -1,2 +1,2 @@
-SELECT p.id, p.title, p.author_id , u.username, u.profile_pic FROM posts p
+SELECT p.id, p.title, p.author_id , u.username, u.profile_pic, p.content FROM posts p
 JOIN users u ON p.author_id = u.id;
